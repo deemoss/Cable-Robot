71250c4f6004f82bbcc38104f4d266b3a86b258e
deemoss <dimos@dimos.biz>
2023-02-01 11:51:16 +0200

    Dimentioning and cookies

--- sim.js
+++ sim.js
@@ -4,8 +4,43 @@ var ctx = c.getContext("2d");
 var img = new Image();
 img.src = 'wall.webp';
 
+// Read coockies
+function getCookie(name) {
+  // Split cookie string and get all individual name=value pairs in an array
+  var cookieArr = document.cookie.split(";");
+  // Loop through the array elements
+  for (var i = 0; i < cookieArr.length; i++) {
+    var cookiePair = cookieArr[i].split("=");
+    // Removing whitespace at the beginning of the cookie name and compare it with the given string
+    if (name == cookiePair[0].trim()) {
+      // Decode the cookie value and return
+      return decodeURIComponent(cookiePair[1]);
+    }
+  }
+  // Return null if not found
+  return null;
+}
 
+
+// Canvas read stored dimentions
+var canvasWidth;
+var canvasHeight;
+
+function setupCanvas() {
+  canvasWidth = getCookie('canvasWidth')
+  if (canvasWidth == null) {
+    canvasWidth = 200;
+  }
+  canvasHeight = getCookie('canvasHeight')
+  if (canvasHeight == null) {
+    canvasHeight = 200;
+  }
+}
+
+setupCanvas();
 window.onload = setBackround;
+
+
 
 var running = false;
 
@@ -17,7 +52,7 @@ var L = {
 
 // Right origin top right
 var R = {
-  x: c.width,
+  x: canvasWidth,
   y: 0
 };
 
@@ -35,8 +70,8 @@ var len_R = 0;
 var theta_L = 0;
 var theta_R = 0;
 
-var x = 0;
-var y = 0;
+var x = canvasWidth / 2;
+var y = canvasHeight / 2;
 var previous_x = x;
 var previous_y = y;
 
@@ -49,19 +84,20 @@ var S = {
 // Parameters
 var lineW = 3
 
-// Canvas
-var canWidth = 600;
-var canHeight = 400;
-
-document.getElementById("myCanvas").width = canWidth;
-document.getElementById("myCanvas").height = canHeight;
 
 
+document.getElementById("myCanvas").width = canvasWidth;
+document.getElementById("myCanvas").height = canvasHeight;
 
+document.getElementById("canv").style.width = canvasWidth + 'px';
 
+document.getElementById("widthID").value = canvasWidth;
+document.getElementById("heightID").value = canvasHeight;
+
+draw();
 
 function setBackround() {
-  ctx.drawImage(img, 0, 0, canWidth, canHeight);
+  ctx.drawImage(img, 0, 0, canvasWidth, canvasHeight);
 }
 
 // Linear Interpolation. Also known as "lerp" or "mix"
@@ -87,8 +123,8 @@ function draw() {
   setBackround();
 
   // Interpolate position od P
-  previous_x = lerp(previous_x, x, 0.5);
-  previous_y = lerp(previous_y, y, 0.5);
+  previous_x = lerp(previous_x, x, 0.05);
+  previous_y = lerp(previous_y, y, 0.05);
 
   // Draw L leg
   ctx.beginPath();
@@ -108,11 +144,28 @@ function draw() {
   ctx.stroke();
   ctx.closePath();
 
-  // Draw square
   ctx.beginPath();
   ctx.lineWidth = lineW;
   ctx.strokeStyle = 'yellow';
-  ctx.fillStyle = "black";
+  ctx.fillStyle = "white";
+  ctx.fillRect(previous_x - S.x / 2, previous_y, S.x, S.y);
+  ctx.strokeRect(previous_x - S.x / 2, previous_y, S.x, S.y);
+  ctx.stroke();
+  ctx.closePath();
+
+  // Draw square
+  var solidColor;
+  if (Math.abs(previous_x - x) < 2) {
+    solidColor = 'white'
+
+  } else {
+    solidColor = 'black'
+  }
+
+  ctx.beginPath();
+  ctx.lineWidth = lineW;
+  ctx.strokeStyle = 'yellow';
+  ctx.fillStyle = solidColor;
   ctx.fillRect(previous_x - S.x / 2, previous_y, S.x, S.y);
   ctx.strokeRect(previous_x - S.x / 2, previous_y, S.x, S.y);
   ctx.stroke();
@@ -123,23 +176,25 @@ function draw() {
 }
 
 async function iteratePositions(delay) {
+  for (let j = 1; j < 10; j++) {
+    P.y = 0 + canvasHeight / 10 * j;
+    for (let i = 1; i < 5; i += 1) {
+      P.x = 0 + canvasWidth / 5 * i;
 
 
-  for (let i = 0; i < 10; i += 1) {
-    P.x = 500 - 50 * i;
-    P.y = 50;
+      // Calculate lengthas from left ancor point (L), right ancor point (R) and the required position point (P)
+      len_L = Math.hypot(Math.abs(P.x - L.x), Math.abs(P.y - L.y))
+      len_R = Math.hypot(Math.abs(P.x - R.x), Math.abs(P.y - R.y))
 
-    // Calculate lengthas from left ancor point (L), right ancor point (R) and the required position point (P)
-    len_L = Math.hypot(Math.abs(P.x - L.x), Math.abs(P.y - L.y))
-    len_R = Math.hypot(Math.abs(P.x - R.x), Math.abs(P.y - R.y))
+      // Calculate angles from left ancor point (L), right ancor point (R) and the required position point (P)
+      theta_L = Math.atan2(Math.abs(P.y - L.y), Math.abs(P.x - L.x)) * 180 / Math.PI;
+      theta_R = Math.atan2(Math.abs(P.y - R.y), Math.abs(P.x - R.x)) * 180 / Math.PI;
 
-    // Calculate angles from left ancor point (L), right ancor point (R) and the required position point (P)
-    theta_L = Math.atan2(Math.abs(P.y - L.y), Math.abs(P.x - L.x)) * 180 / Math.PI;
-    theta_R = Math.atan2(Math.abs(P.y - R.y), Math.abs(P.x - R.x)) * 180 / Math.PI;
+      lineAtAngle();
 
-    lineAtAngle();
-
-    await new Promise(res => setTimeout(res, delay));
+      // Delay step
+      await new Promise(res => setTimeout(res, delay));
+    }
   }
   running = false;
 }
@@ -151,14 +206,14 @@ function start() {
     return;
   } else if (!running) {
 
-    iteratePositions(1000);
+    iteratePositions(1500);
     draw();
     running = true;
   }
 }
 
 function stop() {
-  //location.reload();
+  location.reload();
 }
 
 function save() {
@@ -167,16 +222,47 @@ function save() {
     return;
   }
   // save values
-  canWidth = document.getElementById("widthID").value;
-  canHeight = document.getElementById("heightID").value;
+  canvasWidth = document.getElementById("widthID").value;
+  canvasHeight = document.getElementById("heightID").value;
+  // Write cookies
+  document.cookie = "canvasWidth=" + canvasWidth;
+  document.cookie = "canvasHeight=" + canvasHeight;
   // set canvas size
-  document.getElementById("myCanvas").width = canWidth;
-  document.getElementById("myCanvas").height = canHeight;
+  document.getElementById("myCanvas").width = canvasWidth;
+  document.getElementById("myCanvas").height = canvasHeight;
 
   // update backround picture
   setBackround();
 
+  console.log(canvasHeight);
+
   // Update Right origin X value
-  R.x = canWidth;
+  R.x = canvasWidth;
+  location.reload();
+
 }
 
+
+// Get the input field
+var widthInput = document.getElementById("widthID");
+// Execute a function when the user presses a key on the keyboard
+widthInput.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("btn_save").click();
+  }
+});
+
+var widthInput = document.getElementById("heightID");
+heightID.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("btn_save").click();
+  }
+});
